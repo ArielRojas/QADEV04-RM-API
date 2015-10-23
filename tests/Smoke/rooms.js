14d7a94f0ca30ead6bd8dc21decedb01cafb7f14
BrayanRosas <brayan_rosas@hotmail.com>
2015-10-22 22:51:11 -0400

    rooms refactor v2.1

--- tests/Smoke/rooms.js
+++ tests/Smoke/rooms.js
@@ -1,11 +1,14 @@
 //SmokeRooms
+var init = require('../../init');
 var expect = require('chai').expect;
-var tokenAPI=require('../../lib/tokenAPI');
-var config=require('../../config/config');
-var endPoints=require('../../config/endPoints');
-var resourceConfig = require('../../config/resource.json');
-var roomManagerAPI=require('../../lib/RoomManagerAPI');
-var util=require('../../util/util');
+
+var tokenAPI=require(GLOBAL.initialDirectory+'/lib/tokenAPI');
+var config =require(GLOBAL.initialDirectory+'/config/config.json');
+var endPoints=require(GLOBAL.initialDirectory+'/config/endPoints');
+
+var resourceConfig = require(GLOBAL.initialDirectory+'/config/resource.json');
+var roomManagerAPI=require(GLOBAL.initialDirectory+'/lib/RoomManagerAPI');
+var util=require(GLOBAL.initialDirectory+'/util/util');
 var token=null;
 var room=null;
 var resource=null;
@@ -17,7 +20,7 @@ var endPoint=null;
 
 
 describe('Smoke Testing for Room routes', function() {
-this.timeout(config.timeOut);
+	this.timeout(config.timeOut);
 
 	/**
 	 * Pre condition to execute the set Test Cases.
@@ -31,12 +34,9 @@ this.timeout(config.timeOut);
 			.getToken(function(err,res){
 				token = res.body.token;
 				endPoint=config.url+endPoints.rooms;
-				console.log(endPoint);
 				roomManagerAPI.get(endPoint,function(err,resp){
-
 					randomRoom=util.generateRandom(1,resp.body.length);
 					room=resp.body[randomRoom];
-				
 					done();
 				});
 
@@ -89,7 +89,7 @@ this.timeout(config.timeOut);
 });
 
 describe('Smoke Testing for Room Resources routes ', function() {
-this.timeout(config.timeOut);
+	this.timeout(config.timeOut);
 
 	/**
 	 * Pre condition to execute the set Test Cases.
@@ -99,7 +99,6 @@ this.timeout(config.timeOut);
 	 */
 	before('Before Set',function (done) {
 			process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-		
 		tokenAPI
 			.getToken(function(err,res){
 				token = res.body.token;
@@ -111,38 +110,29 @@ this.timeout(config.timeOut);
 						
 						endPoint=config.url+endPoints.resources
 						json=util.getRandomResourcesJson(resourceConfig.resourceNameSize);
-						roomManagerAPI.post(token,endPoint,json,function(err,resourceRes){
-							resource=resourceRes;
-								var endPoint2=config.url+endPoints.rooms+'/'+room._id+'/resources';
-								var json2={"resourceId": resource.body._id,"quantity": 1 };	
-								
-									roomManagerAPI.post
-										(token,endPoint2,json2,function(err,resAsoc){
-										
-										resourceAsoc=resAsoc;
-										
-										done();
-									});
-															
-						});	
+							roomManagerAPI.post(token,endPoint,json,function(err,resourceRes){
+								resource=resourceRes;
+									var endPoint2=config.url+endPoints.rooms+'/'+room._id+'/resources';
+									var json2={"resourceId": resource.body._id,"quantity": 1 };	
+										roomManagerAPI.post
+											(token,endPoint2,json2,function(err,resAsoc){
+											resourceAsoc=resAsoc;
+							
+											done();
+										});
+																
+							});	
 					});				
-			});
-
-
-		
+			});		
 	});
 
 
 	after('Before Set',function (done) {
-
 		endPoint=config.url+endPoints.resources+'/'+resource.body._id;
 		roomManagerAPI
 			.del(token,endPoint,function(err,resourceDel){
-			
-			done();	
+				done();	
 			});	
-
-
 	});
 
 /**
@@ -152,24 +142,19 @@ this.timeout(config.timeOut);
 	it('GET /rooms/{roomId}/resources,Verify the status 200',function(done){	
 
 		 			endPoint=config.url+endPoints.rooms+'/'+room._id+endPoints.resources;
-		 			console.log(endPoint);
-
 		 			roomManagerAPI.get(endPoint,function(err,res){
-
 		 				expect(res.status).to.equal(200);
 			  			done();
 
-		 			})
-			  		
-		  			 						
+		 			});			  				  			 						
 	});	
 
 /**
  * Smoke Test to the service room with the method POST 
  * for associates the room with an existent resource.
  */
-	it('POST rooms/{:roomId}/resources, Verify the status 200',function(done)
-	{		
+	it('POST rooms/{:roomId}/resources, Verify the status 200',function(done){
+			
 					expect(resourceAsoc.status).to.equal(200);
 			  		done();
 	});
@@ -181,43 +166,42 @@ this.timeout(config.timeOut);
 	it('GET /rooms/{:roomId}/resources/{:roomResourceId}, Verify the status 200',function(done){
 															
 			endPoint=config.url+endPoints.rooms+'/'+room._id+endPoints.resources+'/'+
-					 resourceAsoc.body.resources[0]._id;
-				
+					 resourceAsoc.body.resources[0]._id;		
 				roomManagerAPI.get(endPoint,function(err,res){
-
 					expect(res.status).to.equal(200);	
 					done();
-
 				})
+	});	
 
+ /**
+ * Smoke Test to the service room with the method PUT
+ * for Updating a specific resource from a specific room
+ */			
+	it('PUT /rooms/{:roomId}/resources/{:roomResourceId}, Verify the status 200',function(done){
+				
+				endPoint=config.url+endPoints.rooms+'/'+room._id+endPoints.resources+'/'+
+					 resourceAsoc.body.resources[0]._id;
+					 json={"quantity": 10};
 
-	});				
-
-	it.skip('POST /rooms/{:roomId}/resources/{:roomResourceId}, Verify the status 200',function(done){
-					
-			roomsAPI
-				.editResourceOfRoom
-					(token,room._id,resourceAsoc.body.resources[0]._id,sendJson,function(err,resourceSpec){
-
-						console.log('roomId : '+room._id+' resource id :'
-							+resourceSpec.body);
-						expect(resourceSpec.status).to.equal(200);		
-						done();													
-				});												
-					
+				roomManagerAPI.put(token,endPoint,json,function(err,resp){
+					expect(resp.status).to.equal(200);	
+					done();		
+				});
 	});	
 	
+ /**
+ * Smoke Test to the service room with the method DEL
+ * for Removing a specific resource from a specific room
+ */	
 	it('DELETE /rooms/{:roomId}/resources/{:roomResourceId}, Verify the status 200 ',function(done){
 						
 			endPoint=config.url+endPoints.rooms+'/'+room._id+endPoints.resources+'/'+
 					 resourceAsoc.body.resources[0]._id;
-
 					 roomManagerAPI.del(token,endPoint,function(err,resp){
 					 	expect(resp.status).to.equal(200);	
 						done();			
 
-					 });
-							
+					 });						
 	});	
 
 });
