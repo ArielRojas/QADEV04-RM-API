b24603628c1998023d8ca07ec5654c0725132206
JoaquinGonzales <joaquinjqn2@gmail.com>
2015-10-26 17:05:58 -0400

    to review 10/26/2015

--- tests/CRUD/services.js
+++ tests/CRUD/services.js
@@ -16,15 +16,22 @@ var serviceEndPoint = url+endPoints.services;
 var serviceEndPointPost=serviceEndPoint + serviceConfig.postFilter;;
 var serviceTypes = url+endPoints.serviceTypes;
 var roomEndPoint = serviceEndPoint;
+//this var helps to concat an End point to get an specific room
 var rooms = endPoints.rooms;
 //Global Variables
 var token = null; 
 var idService = 0;
 var idRoom = null;
 var servicefromDB = null;
+var roomsfromDB = null;
+var respService = null;
+var ObjectId = require('mongodb').ObjectID;
 //variables with Json Archives 
 var serviceIdJson = serviceConfig.serviceId;
 var servicejson = serviceConfig.service;
+var roomDisplayJson = serviceConfig.roomDisplayJson;
+var roomProperties = serviceConfig.PropertiesRoom;
+var serviceProperties = serviceConfig.PropertiesService;
 //status for response 200
 var ok = config.httpStatus.Ok;
 
@@ -52,12 +59,12 @@ describe('CRUD Tesinting for Services Room Manager',function()
 			.get(serviceTypes,function(err,res)
 			{
 				expect(res.status).to.equal(ok);
-				expect(res.body[0]).to.have.property('version');
-				expect(res.body[0].version).to.equal('0.0.1');
-				expect(res.body[0]).to.have.property('name');
-				expect(res.body[0].name).to.equal('exchange');
-				expect(res.body[0]).to.have.property('support');
-				expect(res.body[0].support[0]).to.equal('Exchange Server 2010');
+				expect(res.body[0]).to.have.property(serviceProperties.version);
+				expect(res.body[0].version).to.equal(serviceProperties.zero);
+				expect(res.body[0]).to.have.property(serviceProperties.name);
+				expect(res.body[0].name).to.equal(serviceProperties.exchange);
+				expect(res.body[0]).to.have.property(serviceProperties.support);
+				expect(res.body[0].support[0]).to.equal(serviceProperties.Exchange);
 				done();
 
 			});		
@@ -72,17 +79,16 @@ describe('CRUD Tesinting for Services Room Manager',function()
 					.getwithToken(token,serviceEndPoint,function(err,res)
 					{
 						var resp=res.body[0];
-						expect(res.status).to.equal(200);
-						expect(resp).to.have.property('type');
+						expect(res.status).to.equal(ok);
+						expect(resp).to.have.property(serviceProperties.type);
 						expect(resp.type).to.equal(servicefromDB.type);
-						expect(resp).to.have.property('name');
+						expect(resp).to.have.property(serviceProperties.name);
 						expect(resp.name).to.equal(servicefromDB.name);
-						expect(resp).to.have.property('version');
+						expect(resp).to.have.property(serviceProperties.version);
 						expect(resp.version).to.equal(servicefromDB.version);
-						expect(resp).to.have.property('_id');
-						//assert.typeOf(servicefromDB._id, 'HexString');
+						expect(resp).to.have.property(serviceProperties._id);
 						expect(resp._id).to.equal(servicefromDB._id.toString());
-						expect(resp).to.have.property('impersonate');
+						expect(resp).to.have.property(serviceProperties.impersonate);
 						expect(resp.impersonate).to.equal(servicefromDB.impersonate);
 						done();
 					});
@@ -90,51 +96,92 @@ describe('CRUD Tesinting for Services Room Manager',function()
 	});
 	describe('CRUD testing for service/serviceID',function()
 	{
+		
 		beforeEach(function(done)
 		{
 			roomManagerAPI
 				.getwithToken(token,serviceEndPoint,function(err,resp)
 				{
+					respService = resp;
 					idService = resp.body[0]._id;
 					done();
 				});
 		});
 		it('GET /service/serviceID, CRUD testing for an specific service',function(done)
 		{			
+			serviceIdJson._id=ObjectId(idService);
 			mongodb
-				.findDocuments('services',function(res)
+				.findDocument('services',serviceIdJson, function(res)
 				{	
 					servicefromDB = res;
-					roomManagerAPI
-						.getwithToken(token,serviceEndPoint,function(err,res)
-						{
-							var resp = res.body[0];
-							var serFromDB = servicefromDB[0];
-							expect(res.status).to.equal(200);
-							expect(resp).to.have.property('type');
-							expect(resp.type).to.equal(serFromDB.type);
-							expect(resp).to.have.property('name');
-							expect(resp.name).to.equal(serFromDB.name);
-							expect(resp).to.have.property('version');
-							expect(resp.version).to.equal(serFromDB.version);
-							expect(resp).to.have.property('_id');
-							//assert.typeOf(servicefromDB._id, 'string');
-							expect(resp._id).to.equal(serFromDB._id.toString());
-							expect(resp).to.have.property('impersonate');
-							expect(resp.impersonate).to.equal(serFromDB.impersonate);
-							done();
-						});
+					expect(respService.status).to.equal(ok);
+					var respServiceBody = respService.body[0];
+					expect(respServiceBody).to.have.property(serviceProperties.type);
+					expect(respServiceBody.type).to.equal(servicefromDB.type);
+					expect(respServiceBody).to.have.property(serviceProperties.name);
+					expect(respServiceBody.name).to.equal(servicefromDB.name);
+					expect(respServiceBody).to.have.property(serviceProperties.version);
+					expect(respServiceBody.version).to.equal(servicefromDB.version);
+					expect(respServiceBody).to.have.property(serviceProperties._id);
+					expect(respServiceBody._id).to.equal(servicefromDB._id.toString());
+					expect(respServiceBody).to.have.property(serviceProperties.impersonate);
+					expect(respServiceBody.impersonate).to.equal(servicefromDB.impersonate);
+					done();
+	
 				});
 		});
-		it.only('test',function(done)
-		{
-			var ObjectId = require('mongodb').ObjectID;
+		it('GET /services/serviceId/rooms, CRUD testing for Email Server Rooms',function(done)
+		{			
+			roomEndPoint = roomEndPoint +'/'+ idService + rooms;
 			mongodb
-				.findDocument('services',{ "_id": ObjectId(idService) },function(res)
+				.findDocuments('rooms',function(res)
 				{
-					console.log(res)
-					done();
-				})
-		})
+					roomsfromDB = res;
+					roomManagerAPI
+					.get(roomEndPoint,function(err,res)
+					{
+						expect(res.status).to.equal(ok);
+						var responseRooms= res.body;
+						expect(responseRooms.length).to.equal(roomsfromDB.length);
+						expect(responseRooms[0]).to.have.property(roomProperties.emailAddress);
+						expect(responseRooms[0]).to.have.property(roomProperties.displayName);
+						expect(responseRooms[0]).to.have.property(roomProperties.serviceId);
+						expect(responseRooms[0]).to.have.property(roomProperties._id);
+						expect(responseRooms[0]).to.have.property(roomProperties.enabled);
+						expect(responseRooms[0]).to.have.property(roomProperties.locationId);
+						expect(responseRooms[0]).to.have.property(roomProperties.customDisplayName);
+						done();
+					});
+				});
+		});
+		it('Get /service/serviceID/rooms/roomId, CRUD testing for an specific room',function(done)
+		{
+
+			mongodb
+			.findDocument('rooms',roomDisplayJson,function(res)
+			{
+				roomsfromDB = res;
+				roomEndPoint = roomEndPoint +'/'+roomsfromDB._id;
+				roomManagerAPI
+					.get(roomEndPoint,function(err,res)
+					{
+						expect(res.status).to.equal(ok);
+						var responseRoom=res.body;
+						expect(responseRoom).to.have.property(roomProperties.emailAddress);
+						expect(responseRoom.emailAddress).to.equal(roomsfromDB.emailAddress);
+						expect(responseRoom).to.have.property(roomProperties.displayName);
+						expect(responseRoom.displayName).to.equal(roomsfromDB.displayName);
+						expect(responseRoom).to.have.property(roomProperties.serviceId);
+						expect(responseRoom.serviceId).to.equal(roomsfromDB.serviceId.toString());
+						expect(responseRoom).to.have.property(roomProperties._id);
+						expect(responseRoom._id).to.equal(roomsfromDB._id.toString());
+						expect(responseRoom).to.have.property(roomProperties.enabled);
+						expect(responseRoom.enabled).to.equal(roomsfromDB.enabled);
+						expect(responseRoom).to.have.property(roomProperties.locationId);
+						expect(responseRoom.locationId).to.equal(roomsfromDB.locationId);
+						done();
+					});
+			});	
+		});
 	});
 });
