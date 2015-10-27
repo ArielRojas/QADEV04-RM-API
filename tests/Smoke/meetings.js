cc90b53705bebe34c234f516bb0d99131ae4e656
ArielRojas <arielwagnerrojas@gmail.com>
2015-10-27 14:31:50 -0400

    update

--- tests/Smoke/meetings.js
+++ tests/Smoke/meetings.js
@@ -4,12 +4,6 @@
 var init = require('../../init');
 //with config it can use the methods located into the config file
 var config = require(GLOBAL.initialDirectory+'/config/config.json');
-//Smoke testing - GET Meetings
-//Author Ariel Wagner Rojas
-// the next line call the file init.js to declare a global var(GLOBAL.initialDirectory)
-var init = require('../../init');
-//with config it can use the methods located into the config file
-var config = require(GLOBAL.initialDirectory+'/config/config.json');
 var expect = require('chai').expect;
 //with tokenAPI it can use the parameters located into the loginAPI file
 var tokenAPI = require(GLOBAL.initialDirectory+config.path.tokenAPI);
