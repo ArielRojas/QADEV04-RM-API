7d4a566ce56c6ec5119b88b429b352cb7f71dbc7
JoaquinGonzales <joaquinjqn2@gmail.com>
2015-10-28 21:51:18 -0400

    commit with scenarios

--- tests/CRUD/services.js
+++ tests/CRUD/services.js
@@ -13,7 +13,7 @@ var endPoints = require(GLOBAL.initialDirectory+config.path.endPoints);
 //End Points
 var url = config.url;
 var serviceEndPoint = url+endPoints.services;
-var serviceEndPointPost=serviceEndPoint + serviceConfig.postFilter;;
+var serviceEndPointPost = serviceEndPoint + serviceConfig.postFilter;;
 var serviceTypes = url+endPoints.serviceTypes;
 var roomEndPoint = serviceEndPoint;
 //this var helps to concat an End point to get an specific room
@@ -30,6 +30,8 @@ var ObjectId = require('mongodb').ObjectID;
 var serviceIdJson = serviceConfig.serviceId;
 var servicejson = serviceConfig.service;
 var roomDisplayJson = serviceConfig.roomDisplayJson;
+var roomJson = serviceConfig.roomJson;
+var adminJson = config.exchangeAccount;
 var roomProperties = serviceConfig.PropertiesRoom;
 var serviceProperties = serviceConfig.PropertiesService;
 //status for response 200
@@ -97,13 +99,14 @@ describe('CRUD Tesinting for Services Room Manager',function()
 	describe('CRUD testing for service/serviceID',function()
 	{
 		
-		beforeEach(function(done)
+		before(function(done)
 		{
 			roomManagerAPI
 				.getwithToken(token,serviceEndPoint,function(err,resp)
 				{
 					respService = resp;
 					idService = resp.body[0]._id;
+					roomEndPoint = roomEndPoint +'/'+ idService;
 					done();
 				});
 		});
@@ -132,13 +135,13 @@ describe('CRUD Tesinting for Services Room Manager',function()
 		});
 		it('GET /services/serviceId/rooms, CRUD testing for Email Server Rooms',function(done)
 		{			
-			roomEndPoint = roomEndPoint +'/'+ idService + rooms;
+			var roomEndPointrooms = roomEndPoint+ rooms;
 			mongodb
 				.findDocuments('rooms',function(res)
 				{
 					roomsfromDB = res;
 					roomManagerAPI
-					.get(roomEndPoint,function(err,res)
+					.get(roomEndPointrooms,function(err,res)
 					{
 						expect(res.status).to.equal(ok);
 						var responseRooms= res.body;
@@ -161,9 +164,9 @@ describe('CRUD Tesinting for Services Room Manager',function()
 			.findDocument('rooms',roomDisplayJson,function(res)
 			{
 				roomsfromDB = res;
-				roomEndPoint = roomEndPoint +'/'+roomsfromDB._id;
+				var roomEndPointroomid = roomEndPoint+ rooms +'/'+roomsfromDB._id;
 				roomManagerAPI
-					.get(roomEndPoint,function(err,res)
+					.get(roomEndPointroomid,function(err,res)
 					{
 						expect(res.status).to.equal(ok);
 						var responseRoom=res.body;
@@ -182,6 +185,133 @@ describe('CRUD Tesinting for Services Room Manager',function()
 						done();
 					});
 			});	
+		});
+		it('PUT /service/serviceID/roomId/rooms, CRUD testing verify the room is modified with metho put',function(done)
+		{
+			mongodb
+				.findDocument('rooms',roomDisplayJson,function(res)
+				{
+					roomsfromDB = res;
+					var roomEndPointroomId = roomEndPoint+ rooms +'/'+roomsfromDB._id;
+					roomManagerAPI
+						.put(token,roomEndPointroomId,roomJson,function(err,res)
+						{
+							expect(res.status).to.equal(ok);
+							var responseRoom = res.body;
+							expect(responseRoom).to.have.property(roomProperties.emailAddress);
+							expect(responseRoom.emailAddress).to.equal(roomsfromDB.emailAddress);
+							expect(responseRoom).to.have.property(roomProperties.displayName);
+							expect(responseRoom.displayName).to.equal(roomsfromDB.displayName);
+							expect(responseRoom).to.have.property(roomProperties.serviceId);
+							expect(responseRoom.serviceId).to.equal(roomsfromDB.serviceId.toString());
+							expect(responseRoom).to.have.property(roomProperties._id);
+							expect(responseRoom._id).to.equal(roomsfromDB._id.toString());
+							expect(responseRoom).to.have.property(roomProperties.enabled);
+							expect(responseRoom.enabled).to.equal(roomsfromDB.enabled);
+							expect(responseRoom).to.have.property(roomProperties.locationId);
+							expect(responseRoom.locationId).to.equal(roomsfromDB.locationId);
+							//test for the properties that were modified
+							expect(responseRoom).to.have.property(roomProperties.customDisplayName);
+							expect(responseRoom.customDisplayName).to.equal(roomJson.customDisplayName);
+							expect(responseRoom).to.have.property(roomProperties.enabled);
+							expect(responseRoom.enabled).to.equal(false);
+							done();
+						});
+				});
+		});
+	});
+	xdescribe('Test for DELETE Method CRUD testing',function()
+	{
+		before(function(done)
+		{
+			roomManagerAPI
+				.getwithToken(token,serviceEndPoint,function(err,res)
+				{
+					//console.log(res.body);done();
+					idService = res.body;
+					if(idService.length == 0)
+					{
+						serviceEndPointPost=serviceEndPoint+serviceConfig.postFilter;
+						roomManagerAPI
+							.post(token,serviceEndPointPost,adminJson,function(err,resp)
+							{
+								idService=resp.body;
+								done();
+							});
+					}
+					else
+						{
+							done();
+						}
+				});
+		});
+		it('DELETE /services, CRUD testing for DELETE Method',function(done)
+		{			
+			roomManagerAPI
+				.del(token,serviceEndPoint+'/'+idService[0]._id,function(err,res)
+				{
+					var deleteservice = res;
+					serviceIdJson._id = ObjectId(idService[0]._id);
+					mongodb
+						.findDocument('services',serviceIdJson,function(res)
+						{
+							expect(deleteservice.status).to.equal(ok);
+							expect(res).to.equal(undefined);					
+							done();
+						});
+				});
+		});
+	});
+	xdescribe('Test for Post Method CRUD',function()
+	{
+		before(function(done)
+		{
+			roomManagerAPI
+				.getwithToken(token,serviceEndPoint,function(err,res)
+				{
+					//console.log(res.body);done();
+					idService = res.body;
+					if(idService.length > 0)
+					{
+						roomManagerAPI
+							.del(token,serviceEndPoint+'/'+idService[0]._id,function(err,res)
+							{
+								console.log('deleted');
+								done();
+							});
+					}
+					else
+						{
+							done();
+						}
+				});
+		});
+		it('POTS /services, CRUD testing for Post Method',function(done)
+		{
+			serviceEndPointPost=serviceEndPoint+serviceConfig.postFilter;
+			roomManagerAPI
+				.post(token,serviceEndPointPost,adminJson,function(err,resp)
+				{
+					var PostService = resp;
+					serviceIdJson._id=ObjectId(PostService.body[0]._id);
+					mongodb
+						.findDocument('services',serviceIdJson, function(res)
+						{
+						expect(res.status).to.equal(ok);
+						var respServiceBody = PostService.body[0];
+						expect(respServiceBody).to.have.property(serviceProperties.type);
+						expect(respServiceBody.type).to.equal(servicefromDB.type);
+						expect(respServiceBody).to.have.property(serviceProperties.name);
+						expect(respServiceBody.name).to.equal(servicefromDB.name);
+						expect(respServiceBody).to.have.property(serviceProperties.version);
+						expect(respServiceBody.version).to.equal(servicefromDB.version);
+						expect(respServiceBody).to.have.property(serviceProperties._id);
+						expect(respServiceBody._id).to.equal(servicefromDB._id.toString());
+						expect(respServiceBody).to.have.property(serviceProperties.impersonate);
+						expect(respServiceBody.impersonate).to.equal(servicefromDB.impersonate);
+						done();
+					});
+				});
 		});
 	});
 });
